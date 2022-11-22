36f0b4feae056420b1e72c06e5cdf32dca6094d4
BlackErik <blac.erik@gmail.com>
2022-11-21 18:09:22 -0700

    more complex JSX, basic css styling

--- starting-setup/src/components/ExpenseItem.js
+++ starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div className="expense-item">
+      <div>November 21st 2022</div>
+      <div className="expense-item-description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item-price">$294.68</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
