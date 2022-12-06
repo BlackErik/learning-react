3164a8a708d58f861ca393e16f00c8326d9cd7dc
BlackErik <blac.erik@gmail.com>
2022-12-05 20:35:29 -0700

    learning about two way binding

--- starting-setup/src/components/NewExpense/ExpenseForm.js
+++ starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -49,6 +49,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -56,7 +59,11 @@ const ExpenseForm = () => {
       <div className="new-expense-controls">
         <div className="new-expense-control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense-control">
           <label>Amount</label>
@@ -64,12 +71,13 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
         <div className="new-expense-control">
           <label>Date</label>
-          <input type="date" onChange={dateChangeHandler} />
+          <input type="date" value={enteredDate} onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense-actions">
