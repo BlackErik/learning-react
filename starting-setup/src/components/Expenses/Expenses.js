25bc125fc07ae0e90b441aba6751b9c6d5dd58d4
BlackErik <blac.erik@gmail.com>
2022-12-06 21:11:54 -0700

    filtering by year is now working

--- starting-setup/src/components/Expenses/Expenses.js
+++ starting-setup/src/components/Expenses/Expenses.js
@@ -5,18 +5,23 @@ import "./Expenses.css";
 import { useState } from "react";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2022");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
